ec4c3abab0d9d69680c41ae4b01648ecb4f92928
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 21:50:34 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,6 +1,8 @@
 def levitation_quiz
-loop do
-  puts "What is the spell that enacts levitation?"
+  loop do 
+      puts "What is the spell tht enacts levitation?"
+      answer = gets.chomp
+      break if answer == "Wingardium Levio"
+    end
+    puts "You passed the quiz!"
 end
-
-
