a9c8f40cb764d5d508aecb7fb1f66c7ee253356a
magical-flow-3186 <rregalado17@gmail.com>
2019-06-01 21:48:06 +0000

    Automatically backed up by Learn

--- levitation_quiz.rb
+++ levitation_quiz.rb
@@ -1,12 +1,5 @@
 def levitation_quiz
-    loop do 
-        puts "What is the spell that enacts levitation"
-        levitation_quiz = gets.chomp
-        break if levitation_quiz == "STOP"
-    end
-    puts "You passed the quiz!"
-  end
-levitation_quiz
+
 end
 
 
